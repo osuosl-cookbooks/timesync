29d41f51e75709380241ce34aad389d7e8558f22
Evan Tschuy <tschuy@osuosl.org>
2015-08-26 12:04:24 -0700

    Initial commit of timesync cookbook

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+timesync CHANGELOG
+==================
+This file is used to list changes made in each version of the
+timesync cookbook.
+
+0.1.0
+-----
+- Initial release of timesync
+
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'timesync'
+maintainer       'Oregon State University'
+maintainer_email 'chef@osuosl.org'
+license          'apache2'
+description      'Installs/Configures timesync'
+long_description 'Installs/Configures timesync'
+version          '0.1.0'
+
