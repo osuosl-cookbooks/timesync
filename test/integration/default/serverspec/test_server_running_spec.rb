31242b45835d52996fc82f2f92187a54b46dfa9e
Evan Tschuy <tschuy@osuosl.org>
2015-08-28 10:42:27 -0700

    check ownership of database

--- test/integration/default/serverspec/test_server_running_spec.rb
+++ test/integration/default/serverspec/test_server_running_spec.rb
@@ -14,4 +14,6 @@ end
 # database exists
 describe file('/opt/timesync/source/dev.sqlite3') do
   it { should exist }
+  it { should be_grouped_into 'timesync' }
+  it { should be_owned_by 'timesync' }
 end
