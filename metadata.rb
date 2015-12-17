5a5189aa7aa3dd7d1e6813293f044c7effa813e0
armiller <armiller@osuosl.org>
2015-12-17 15:47:20 -0800

    Bump cookbook

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'chef@osuosl.org'
 license          'apache2'
 description      'Installs/Configures timesync'
 long_description 'Installs/Configures timesync'
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'build-essential'
 depends 'database'
