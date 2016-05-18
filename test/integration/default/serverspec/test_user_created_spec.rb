5809c2a3ee84eb9475d16b2041c668de5cb44ad4
Tristan Patch <patcht@osuosl.org>
2016-05-18 12:29:00 -0700

    Fixed issue with spacing and quotes. No more rubocop issues or test failures.

--- test/integration/default/serverspec/test_user_created_spec.rb
+++ test/integration/default/serverspec/test_user_created_spec.rb
@@ -1,18 +1,18 @@
 require 'serverspec'
 set :backend, :exec
 
-root_pass = "dLj+87o0fyYqPsbvlNuigpA/XFI9ThJAFjMMibEypDoJpAN0hMqBVs03fptaubFL \
-CHBAh2mZ6lw0nlGwXhHMSQ"
+root_pass = 'dLj+87o0fyYqPsbvlNuigpA/XFI9ThJAFjMMibEypDoJpAN0hMqBVs03fptaubFL'\
+            'CHBAh2mZ6lw0nlGwXhHMSQ'
 
-describe command("curl -d '{ \
-                             \"auth\": { \
-                               \"type\": \"password\", \
-                               \"username\": \"root\", \
-                               \"password\": \"#{root_pass}\" \
-                             } \
-                           }' \
-                       -H \"Content-Type: application/json\" http://localhost \
-                                                             /v0/login") do
+describe command("curl -d '{"\
+                            '"auth": {'\
+                              '"type": "password", '\
+                              '"username": "root", '\
+                              "\"password\": \"#{root_pass}\""\
+                            '}'\
+                          "}' "\
+                      '-H "Content-Type: application/json" http://localhost'\
+							    '/v0/login') do
   its(:stdout) { should match(%r'^{\"token\":\s?\"[a-zA-Z0-9+/=.]+\"}$') }
   its(:exit_status) { should eq 0 }
 end
