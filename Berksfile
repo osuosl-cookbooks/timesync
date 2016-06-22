63840b3f65642053d26517dba1da256ba9275353
fahlmant <fahlmantaylor@gmail.com>
2016-06-22 11:59:32 -0700

    Moved create databse recipe to test cookbook

--- Berksfile
+++ Berksfile
@@ -2,4 +2,5 @@ source 'https://supermarket.chef.io'
 
 cookbook 'haproxy'
 cookbook 'nodejs-webapp', git: 'git@github.com:osuosl-cookbooks/nodejs-webapp'
+cookbook 'timesync-test', path: './test/integration/cookbooks/timesync-test'
 metadata
---
+++ test/integration/cookbooks/timesync-test/metadata.rb
@@ -0,0 +1,7 @@
+name             'timesync-test'
+maintainer       'Oregon State University'
+maintainer_email 'systems@osuosl.org'
+license          'Apache 2.0'
+description      'Installs/Configures timesync-test'
+long_description ''
+version          '0.1.0'
